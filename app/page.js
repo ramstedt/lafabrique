47042bab88ada46dcbcd8a093137685de0e71a5d
Emma Ramstedt <71063351+emmaramstedt@users.noreply.github.com>
2025-02-08 22:41:30 +0100

    flower bg + border tests

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Navbar from '@/components/Navbar/Navbar';
 import styles from './page.module.css';
 import Hero from '@/components/Hero/Hero';
 import { fetchData } from '@/utils/fetchLandingPage';
+import Image from 'next/image';
 
 export default async function Home() {
   const { data } = await fetchData();
@@ -13,7 +14,24 @@ export default async function Home() {
       <main className={styles.main}>
         hej
         <br />
-        hej
+        <div className={styles.imageWrapper}>
+          <Image
+            src='/assets/camomile11.svg'
+            width={500}
+            height={500}
+            alt='Camomile flower sketch'
+            className={styles.bg}
+            priority
+          />
+        </div>
+        <div className={styles.container}>
+          <div className={styles.border}>
+            <div className={styles.spaceAround}>
+              <p>I am an element with a hand-drawn border!</p>
+              <p>No animation applied.</p>
+            </div>
+          </div>
+        </div>
         <br />
         hej
         <br />
@@ -31,8 +49,14 @@ export default async function Home() {
         <br />
         hej
         <br />
-        hej
-        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores quo
+          natus at ad rem fuga. Dolor, tempora labore! Aut dolores temporibus
+          natus harum pariatur beatae optio rerum dolorem hic unde quasi
+          consequatur at quisquam, cumque, culpa vitae est possimus itaque qui
+          consequuntur dolorum explicabo! Repudiandae est provident voluptas qui
+          iste.
+        </p>
         hej
         <br />
         hej
