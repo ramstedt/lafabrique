e2369909f03d0b2342150ee01af8285911fc8071
ramstedt <ceramstedt@gmail.com>
2025-02-25 15:00:23 +0100

    pages setup

---
+++ app/hyra/page.js
@@ -0,0 +1,20 @@
+import Navbar from '@/components/Navbar/Navbar';
+import Hero from '@/components/Hero/Hero';
+import { fetchData } from '@/utils/fetchLandingPage';
+import Image from 'next/image';
+import Footer from '@/components/Footer/Footer';
+import imageUrlBuilder from '@sanity/image-url';
+import { client } from '@/sanity/sanity';
+import InfoCard from '@/components/InfoCard/InfoCard';
+import ContentBlock from '@/components/ContentBlock/ContentBlock';
+import ArtistCard from '@/components/ArtistCard/ArtistCard';
+
+export default async function hyra() {
+  return (
+    <>
+      <Navbar />
+      <main>Kommer snart</main>
+      <Footer data={data.footer} />
+    </>
+  );
+}
