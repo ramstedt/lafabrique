a145a9f95555c418b8f2c82b107adf772fa6021c
Emma Ramstedt <71063351+emmaramstedt@users.noreply.github.com>
2025-03-05 23:05:00 +0100

    add vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import Footer from '@/components/Footer/Footer';
 import { fetchFooter } from '@/utils/fetchFooter';
 import Navbar from '@/components/Navbar/Navbar';
 import Image from 'next/image';
+import { Analytics } from '@vercel/analytics/react';
 
 const manrope = Manrope({
   subsets: ['latin'],
@@ -17,20 +18,21 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   const { data } = await fetchFooter();
   return (
-    <html lang="sv">
+    <html lang='sv'>
       <body className={manrope.className}>
         <Navbar />
-        <div className="bgImageWrapper">
+        <div className='bgImageWrapper'>
           <Image
-            src="/assets/camomile11-min.svg"
+            src='/assets/camomile11-min.svg'
             width={500}
             height={500}
-            alt="Camomile flower sketch"
-            className="bgImage"
+            alt='Camomile flower sketch'
+            className='bgImage'
             priority
           />
         </div>
         {children} <Footer data={data.footer} />
+        <Analytics />
       </body>
     </html>
   );
