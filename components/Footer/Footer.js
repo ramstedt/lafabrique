678f361854782828c7f79520f0f9fee463432cda
Emma Ramstedt <emma.ramstedt@toxic.se>
2025-03-12 17:20:27 +0100

    adjust grid on footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -22,18 +22,18 @@ export default function Footer({ data }) {
       <div className={styles.footerbg}></div>
       <div className={styles.emptyleft}></div>
       <div className={styles.circle}>
-        <svg viewBox='0 0 200 200' className={styles.svgText}>
+        <svg viewBox="0 0 200 200" className={styles.svgText}>
           <defs>
             <path
-              id='circlePath'
-              d='M 100, 100
+              id="circlePath"
+              d="M 100, 100
                m -80, 0
                a 80,80 0 1,1 160,0
-               a 80,80 0 1,1 -160,0'
+               a 80,80 0 1,1 -160,0"
             />
           </defs>
-          <text fill='#fbf8ef' className={styles.text}>
-            <textPath href='#circlePath' startOffset='50%' textAnchor='middle'>
+          <text fill="#fbf8ef" className={styles.text}>
+            <textPath href="#circlePath" startOffset="50%" textAnchor="middle">
               LA FABRIQUE
             </textPath>
           </text>
@@ -89,15 +89,15 @@ export default function Footer({ data }) {
           <div className={styles.socials}>
             <div className={styles.socialsHeader}>
               {' '}
-              <h3>Följ oss på sociala medier</h3>
+              <h3>Följ oss på sociala medier:</h3>
             </div>
             {data.instagram && (
-              <Link href={data.instagram} target='_blank'>
+              <Link href={data.instagram} target="_blank">
                 <RiInstagramFill />
               </Link>
             )}
             {data.facebook && (
-              <Link href={data.facebook} target='_blank'>
+              <Link href={data.facebook} target="_blank">
                 <RiFacebookCircleFill />
               </Link>
             )}
@@ -106,7 +106,7 @@ export default function Footer({ data }) {
       </div>
       <div className={styles.map}>
         <div>
-          <iframe src='https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Karl%20Johansgatan%20152+(La%20Fabrique)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed'></iframe>
+          <iframe src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Karl%20Johansgatan%20152+(La%20Fabrique)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
         </div>
       </div>
       <div className={styles.infoRight}></div>
